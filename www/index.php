57e0e72760d1c6867b57b468d8a8c53fe35f7a3f
Tor Gjerde <tor.gjerde@uninett.no>
2014-10-06 15:51:33 +0200

    Handling access levels

--- www/index.php
+++ www/index.php
@@ -58,22 +58,25 @@ include './_header.php' ;
 echo '<div id="giza-view">'."\n";
 
 foreach(\uninett\giza\secret\Secret::getSecretsForProfile() as $secret) {
+	$accesslevel = (($secret->getMetadata()->hasPermission(6))?(($secret->getMetadata()->hasPermission(7))?3:2):1) ;
 	echo '    <div>
         <div><div>
-            <img src="static/gfx/icon-menu-3.svg" alt="">
+            <img src="static/gfx/icon-menu-' . $accesslevel . '.svg" alt="">
             <div>';
 #	TODO: gray out meaningless choices
 	echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'read', 'method' => 'view']) . '"><span><img src="static/gfx/icon-view.svg" alt=""></span><span>Decrypt &amp; view</span></a>';
 	echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'read', 'method' => 'save']) . '"><span><img src="static/gfx/icon-download.svg" alt=""></span><span>Decrypt &amp; save plaintext</span></a>';
 #	echo '<a href=""><span><img src="static/gfx/icon-update.svg" alt=""></span><span>Update with new users</span></a>';
-#	TODO: display only if access level >= 2
-	echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'write', 'method' => 'edit']) . '"><span><img src="static/gfx/icon-edit.svg" alt=""></span><span>Edit</span></a>';
-	echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'write', 'method' => 'upload']) . '"><span><img src="static/gfx/icon-upload.svg" alt=""></span><span>Upload new version</span></a>';
-#	echo '<a href=""><span><img src="static/gfx/icon-revert.svg" alt=""></span><span>Revert to previous version</span></a>';
-#	echo '<a href=""><span><img src="static/gfx/icon-history.svg" alt=""></span><span>Full history</span><span><img src="gfx/icon-more.svg" alt=""></span></a>';
-#	TODO: display only if access level >= 3
-#	echo '<a href=""><span><img src="static/gfx/icon-gears.svg" alt=""></span><span>Users &amp; properties</span><span><img src="gfx/icon-more.svg" alt=""></span></a>';
-#	echo '<a href=""><span><img src="static/gfx/icon-delete.svg" alt=""></span><span>Delete</span></a>';
+	if ($accesslevel >= 2) {
+		echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'write', 'method' => 'edit']) . '"><span><img src="static/gfx/icon-edit.svg" alt=""></span><span>Edit</span></a>';
+		echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'write', 'method' => 'upload']) . '"><span><img src="static/gfx/icon-upload.svg" alt=""></span><span>Upload new version</span></a>';
+#		echo '<a href=""><span><img src="static/gfx/icon-revert.svg" alt=""></span><span>Revert to previous version</span></a>';
+#		echo '<a href=""><span><img src="static/gfx/icon-history.svg" alt=""></span><span>Full history</span><span><img src="gfx/icon-more.svg" alt=""></span></a>';
+		if ($accesslevel == 3) { 
+#		echo '<a href=""><span><img src="static/gfx/icon-gears.svg" alt=""></span><span>Users &amp; properties</span><span><img src="gfx/icon-more.svg" alt=""></span></a>';
+#		echo '<a href=""><span><img src="static/gfx/icon-delete.svg" alt=""></span><span>Delete</span></a>';
+			}
+		}
 	echo '</div>
         </div></div>
         <div>
@@ -87,7 +90,7 @@ foreach(\uninett\giza\secret\Secret::getSecretsForProfile() as $secret) {
             <!-- status box icon -->
             <div><!-- status box --></div>
         </div></div>
-    </div>' ;
+    </div>' . "\n" ;
 	}
 
 echo '</div>'."\n";
