e5857c7dd367df8d5533f6f93e7a070284967d60
Tor Gjerde <tor.gjerde@uninett.no>
2014-10-07 09:16:04 +0200

    colouring based on availability

--- www/index.php
+++ www/index.php
@@ -5,6 +5,8 @@
  * @copyright Copyright (c) 2014, UNINETT
  */
 
+use \uninett\giza\secret\Secret ;
+
 function o($str) { return htmlspecialchars($str); }
 function qs($arr) { return o(http_build_query($arr)); }
 
@@ -14,7 +16,7 @@ if ($uploaded = file_get_contents('php://input')) {
 		if (isset($_SERVER['HTTP_X_GIZA_NAME'])) {
 			$params['name'] = $_SERVER['HTTP_X_GIZA_NAME'];
 		}
-		\uninett\giza\secret\Secret::addSecret($uploaded, $params);
+		Secret::addSecret($uploaded, $params);
 		header('Content-Type: application/json', true, 200);
 		die(json_encode([
 			'result' => '200 OK',
@@ -41,7 +43,7 @@ if ($uploaded = file_get_contents('php://input')) {
 		], JSON_PRETTY_PRINT) . "\n");
 	}
 } elseif (isset($_GET['uuid'])) {
-	\uninett\giza\secret\Secret::getSecret($_GET['uuid'])->generateOutput($_GET);
+	Secret::getSecret($_GET['uuid'])->generateOutput($_GET);
 	exit;
 } elseif ($_SERVER['QUERY_STRING']) {
 	header('Location: ' . (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off' ? 'http' : 'https')
@@ -57,23 +59,37 @@ include './_header.php' ;
 
 echo '<div id="giza-view">'."\n";
 
-foreach(\uninett\giza\secret\Secret::getSecretsForProfile() as $secret) {
-	$accesslevel = (($secret->getMetadata()->hasPermission(6))?(($secret->getMetadata()->hasPermission(7))?3:2):1) ;
-	echo '    <div>
+foreach(Secret::getSecretsForProfile() as $secret) {
+	$accesslevel = (($secret->getMetadata()->hasPermission(Secret::ACCESS_WRITE))?
+		(($secret->getMetadata()->hasPermission(Secret::ACCESS_ADMIN))?3:2):1) ;
+#	TODO: IsReadableByProfile
+	$readable = !$readable ;
+	echo '    <div' . (($readable)?'':' class="unavailable"') . '>
         <div><div>
             <img src="static/gfx/icon-menu-' . $accesslevel . '.svg" alt="">
             <div>';
-#	TODO: gray out meaningless choices
-	echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'read', 'method' => 'view']) . '"><span><img src="static/gfx/icon-view.svg" alt=""></span><span>Decrypt &amp; view</span></a>';
-	echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'read', 'method' => 'save']) . '"><span><img src="static/gfx/icon-download.svg" alt=""></span><span>Decrypt &amp; save plaintext</span></a>';
+	$link = (($readable)?' href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'read', 'method' => 'view']) . '"':'');
+	echo '<a' . $link . '><span><img src="static/gfx/icon-view.svg" alt=""></span><span>Decrypt &amp; view</span></a>';
+
+	$link = (($readable)?' href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'read', 'method' => 'save']) . '"':'');
+	echo '<a' . $link . '><span><img src="static/gfx/icon-download.svg" alt=""></span><span>Decrypt &amp; save plaintext</span></a>';
+
 #	echo '<a href=""><span><img src="static/gfx/icon-update.svg" alt=""></span><span>Update with new users</span></a>';
+
 	if ($accesslevel >= 2) {
-		echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'write', 'method' => 'edit']) . '"><span><img src="static/gfx/icon-edit.svg" alt=""></span><span>Edit</span></a>';
-		echo '<a href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'write', 'method' => 'upload']) . '"><span><img src="static/gfx/icon-upload.svg" alt=""></span><span>Upload new version</span></a>';
+		$link = (($readable)?' href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'write', 'method' => 'edit']) . '"':'');
+		echo '<a' . $link . '><span><img src="static/gfx/icon-edit.svg" alt=""></span><span>Edit</span></a>';
+
+		$link = ' href="?' . qs(['uuid' => $secret->getUUID(), 'action' => 'write', 'method' => 'upload']) . '"';
+		echo '<a' . $link . '><span><img src="static/gfx/icon-upload.svg" alt=""></span><span>Upload new version</span></a>';
+
 #		echo '<a href=""><span><img src="static/gfx/icon-revert.svg" alt=""></span><span>Revert to previous version</span></a>';
+
 #		echo '<a href=""><span><img src="static/gfx/icon-history.svg" alt=""></span><span>Full history</span><span><img src="gfx/icon-more.svg" alt=""></span></a>';
+
 		if ($accesslevel == 3) { 
 #		echo '<a href=""><span><img src="static/gfx/icon-gears.svg" alt=""></span><span>Users &amp; properties</span><span><img src="gfx/icon-more.svg" alt=""></span></a>';
+
 #		echo '<a href=""><span><img src="static/gfx/icon-delete.svg" alt=""></span><span>Delete</span></a>';
 			}
 		}
