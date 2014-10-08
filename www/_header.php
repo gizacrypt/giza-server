4ab1e5d67783dc9c586ad91517565cb2f1ebd4fc
Tor Gjerde <tor.gjerde@uninett.no>
2014-10-08 13:46:07 +0200

    including all stylesheets

--- www/_header.php
+++ www/_header.php
@@ -4,6 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title><?php echo htmlspecialchars($title) ?></title>
 <link rel="icon" type="image/svg+xml" href="static/gfx/icon-giza.svg">
+<link rel="stylesheet" type="text/css" href="static/css/default.css">
 <link rel="stylesheet" type="text/css" href="static/css/giza.css">
 <link rel="stylesheet" type="text/css" href="static/css/view.css">
 </head>
